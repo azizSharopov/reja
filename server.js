2f2e846b8d6a36f07ed3aef7f7f44268909985dd
azizSharopov <sharopovaziz23@gmail.com>
2023-01-04 14:53:25 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 console.log("Web serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 //1 Kirish code
 app.use(express.static("public"));
 app.use(express.json());
@@ -14,11 +24,14 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4 Routing code
-app.get("/hello", function (req, res) {
-  res.end(`<h1>HELLO WORLD</h1>`);
+app.post("/create-item", (req, res) => {
+  //TODO: code with db here
 });
-app.get("/gift", function (req, res) {
-  res.end(`<h1>Siz sovg'alar bo'limidasiz</h1>`);
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
